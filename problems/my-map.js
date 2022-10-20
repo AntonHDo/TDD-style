1072f96febb9082aeefa9f49eb69dfc1b13f2705
anton <antonhdo@gmail.com>
2022-10-20 16:07:27 -0700

    map

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,6 +1,10 @@
-function myMap(inputArray, callback) {
+function myMap(inputArray, cb) {
   // Your code here
-  
-};
+  let res = []
+  for (let i = 0; i < inputArray.length; i++) {
+    res.push(cb(inputArray[i]))
+  }
+  return res
+}
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,16 @@
-// Your code here
+const chai = require("chai");
+const spies = require("chai-spies");
+const expect = chai.expect;
+chai.use(spies);
+
+const myMap = require("../problems/my-map")
+
+let arr;
+beforeEach(() => {
+    arr = [2, 4, 6]
+})
+describe("myMap", () => {
+    it('Should return an array passing each item through the callback', () => {
+        expect(myMap(arr, (item) => item += 1)).to.eql([3, 5, 7])
+    })
+})
