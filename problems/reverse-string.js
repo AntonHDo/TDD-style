40688c5dcd6d6d571d4d682c5148639a5dc540f1
Aliyah Indra <aliyah.indra@gmail.com>
2022-10-20 14:16:06 -0700

    reverse-string

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,10 @@
-module.exports = function reverseString(string) {
+function reverseString(string) {
   // Your code here
-};
+
+  let reversed = string.split("").reverse().join("")
+  return reversed
+
+}
+console.log(reverseString("fun"))
+
+module.exports = reverseString
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,16 @@
 // Your code here
+
+const chai = require("chai");
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+
+const reverseString = require("../problems/reverse-string");
+
+describe("reverseString()", function() {
+    it("should return string reversed", function() {
+        let reversed = reverseString("fun")
+        let expected = "nuf"
+    expect(reversed).to.eql(expected)
+  });
+});
