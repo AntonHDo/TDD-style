0eba57d866936620b1587db71b98dc157834e495
anton <antonhdo@gmail.com>
2022-10-20 15:27:09 -0700

    positive feelings about this next one

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,10 +1,35 @@
 function returnsThree() {
-  // Your code here
+  return 3
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (n < 1 || n > 1000000) {
+    throw new TypeError
+  }
+  if (n > 0 || n < 1000001) {
+    return 1 / n
+  } else {
+  }
 }
+
+
+
+// try {
+//   reciprocal(4)
+//   reciprocal(0)
+//   reciprocal(1111111)
+// } catch {
+//   if (error instanceof TypeError) {
+//     console.log(error.name + ": " + error.message)
+//   }
+// }
+
+
+
+
+
+
+
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,52 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+
+const { returnsThree, reciprocal } = require("../problems/number-fun");
+
+describe("returnsThree()", function () {
+    it("should return the number 3", function () {
+        expect(returnsThree()).to.equal(3)
+    });
+    //     describe("reciprocal()", function () {
+    //         it("should return reciprocal of num", function () {
+    //             expect(reciprocal(6)).to.equal(1 / 6)
+    //             // expect(reciprocal(0)).to.throw(TypeError)
+    //         })
+    //     })
+    //     context("Invalid Arguments", () => {
+    //         it("Should throw error", () => {
+    //             expect(() => reciprocal(-10)).to.throw(TypeError);
+    //             expect(() => reciprocal(99999999)).to.throw(TypeError);
+    //         })
+    //     })
+    // })
+    describe("reciprocal", () => {
+        context("Valid Arguments", () => {
+            it("Should return reciprocal", () => {
+                expect(reciprocal(5)).to.equal(1 / 5);
+                expect(reciprocal(9876)).to.equal(1 / 9876);
+            })
+        })
+
+        context("Invalid Arguments", () => {
+            it("Should throw error", () => {
+                expect(() => reciprocal(-10)).to.throw(TypeError);
+                expect(() => reciprocal(99999999)).to.throw(TypeError);
+            })
+        })
+    })
+
+})
+
+
+
+
+// describe("reverseString()", function () {
+//     it("should return string reversed", function () {
+//         let reversed = reverseString("fun")
+//         let expected = "nuf"
+//         expect(reversed).to.eql(expected)
+//     });
+// });
